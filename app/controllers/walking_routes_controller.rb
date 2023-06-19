9e0e5a220bdafb4eadade2bec88ae0e5411dda32
kotree420 <kouh.aok.04@gmail.com>
2023-06-19 22:53:16 +0900

    style: #3 flashの色を修正。エラー類は赤系、通知類は緑系に統一。

--- app/controllers/walking_routes_controller.rb
+++ app/controllers/walking_routes_controller.rb
@@ -16,11 +16,11 @@ class WalkingRoutesController < ApplicationController
     @walking_route = WalkingRoute.new(walking_route_params)
     if @walking_route.save
       session[:walking_route].clear if session[:walking_route]
-      flash[:success] = ["ルート作成が完了しました"]
+      flash[:info] = ["ルート作成が完了しました"]
       redirect_to action: :show, id: @walking_route.id
     else
       session[:walking_route] = @walking_route
-      flash[:secondary] = @walking_route.errors.full_messages
+      flash[:warning] = @walking_route.errors.full_messages
       redirect_to action: :new
     end
   end
