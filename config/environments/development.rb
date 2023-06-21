d315499a37389d15b2c07fbd3cfc9e1abc81f6b4
kotree420 <kouh.aok.04@gmail.com>
2023-06-21 19:52:54 +0900

    feat: #3 パスワードリセット機能でのGmail送信設定を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,9 +37,21 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'gmail.com',
+    :user_name => 'walkbreak.app@gmail.com',
+    :password => Rails.application.credentials.mailer[:password],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
