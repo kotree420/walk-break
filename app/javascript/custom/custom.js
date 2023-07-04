445b498506dfbefb72b3666318e4d485847c2cc4
kotree420 <kouh.aok.04@gmail.com>
2023-07-05 02:48:25 +0900

    fix: #3 画像アップローダー用のJavaScriptのloadイベントが全画面で定義されてしまっておりエラーが起きていたため、対象画面を絞る形で修正

--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -1,12 +1,14 @@
-window.addEventListener("load", () => {
-  const uploader = document.querySelector(".image-uploader");
-  uploader.addEventListener("change", () => {
-    const upload_file = uploader.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(upload_file);
-    reader.addEventListener("load", () => {
-      const upload_image = reader.result;
-      document.querySelector(".edit-profile-image").setAttribute("src", upload_image);
+if (document.querySelector(".image-uploader")) {
+  window.addEventListener("load", () => {
+    const uploader = document.querySelector(".image-uploader");
+    uploader.addEventListener("change", () => {
+      const upload_file = uploader.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(upload_file);
+      reader.addEventListener("load", () => {
+        const upload_image = reader.result;
+        document.querySelector(".edit-profile-image").setAttribute("src", upload_image);
+      });
     });
   });
-});
+};
