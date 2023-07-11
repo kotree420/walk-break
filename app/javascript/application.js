17553e177d9d787bdc9f969a24d7239e0b1cef4a
kotree420 <kouh.aok.04@gmail.com>
2023-07-11 18:27:44 +0900

    fix: #3 fontawesomeの動作に不具合があったため、CDN読み込みに変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
 // Entry point for the build script in your package.json
-import '@fortawesome/fontawesome-free/js/all';
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
