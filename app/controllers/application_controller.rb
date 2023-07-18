c8f86a5597f44fd953563e57283cfa783b6d628c
kotree420 <kouh.aok.04@gmail.com>
2023-07-19 04:32:38 +0900

    feat #5 散歩ルート編集機能を実装。また他のビューも合わせてid/クラス名などを修正。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
     # name,profile_imageはdevise外のprofilesコントローラーでupdateするためここでは記載なし
     devise_parameter_sanitizer.permit(:account_update, keys: [:email])
   end
+
+  def edit_walking_route_session_clear
+    session[:walking_route_edit].clear if session[:walking_route_edit]
+  end
 end
--- app/controllers/walking_routes_controller.rb
+++ app/controllers/walking_routes_controller.rb
@@ -1,5 +1,6 @@
 class WalkingRoutesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :edit_walking_route_session_clear, except: [:edit]
 
   def index
   end
@@ -33,9 +34,21 @@ class WalkingRoutesController < ApplicationController
   end
 
   def edit
+    @user = current_user
+    @walking_route = session[:walking_route_edit].present? ? WalkingRoute.new(session[:walking_route_edit]) : WalkingRoute.find(params[:id])
   end
 
   def update
+    @walking_route = WalkingRoute.find(params[:id])
+    if @walking_route.update(walking_route_update_params)
+      session[:walking_route_edit].clear if session[:walking_route_edit]
+      flash[:info] = ["散歩ルート情報の更新が完了しました"]
+      redirect_to action: :show
+    else
+      session[:walking_route_edit] = @walking_route
+      flash[:warning] = @walking_route.errors.full_messages
+      redirect_to action: :edit
+    end
   end
 
   def destroy
@@ -44,8 +57,15 @@ class WalkingRoutesController < ApplicationController
   private
 
   def walking_route_params
-    params.require(:walking_route).
-      permit(:name, :comment, :distance, :duration,
+    params
+      .require(:walking_route)
+      .permit(:name, :comment, :distance, :duration,
+        :start_address, :end_address, :encorded_path, :user_id)
+  end
+
+  def walking_route_update_params
+    params
+      .permit(:name, :comment, :distance, :duration,
         :start_address, :end_address, :encorded_path, :user_id)
   end
 end
