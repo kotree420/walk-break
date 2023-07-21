713223ad3b4ca21b35a8461754c6181092ae8973
kotree420 <kouh.aok.04@gmail.com>
2023-07-21 21:08:23 +0900

    test #5 WalkingRouteのModelSpecにバリデーションのテストパターンを追加

--- app/validators/with_line_break_validator.rb
+++ app/validators/with_line_break_validator.rb
@@ -2,6 +2,6 @@ class WithLineBreakValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     # ビューのformでの改行コードとRailsでの改行コードの文字数の違いに対応する
     current_text_length = value&.gsub(/\r\n/, "\n")&.length
-    record.errors.add(attribute, "#{options[:maximum]}文字以内で入力してください") if current_text_length > options[:maximum]
+    record.errors.add(attribute, "#{options[:maximum]}文字以内") if current_text_length > options[:maximum]
   end
 end
