14676e339945a4ce9af758430fd33531183e0f13
kotree420 <kouh.aok.04@gmail.com>
2023-08-07 21:28:05 +0900

    feat #11 プロフィールデザインの変更、scssファイルでのclass名のカテゴリ分けを整理

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -16,8 +16,8 @@ class ProfilesController < ApplicationController
 
   def show
     if @user = User.find_by(id: params[:id])
-      @bookmarked_walking_routes = @user.bookmarked_walking_routes.includes(:user, bookmarks: :user)
       @walking_routes = @user.walking_routes.latest.includes(:user, bookmarks: :user)
+      @bookmarked_walking_routes = @user.bookmarked_walking_routes.includes(:user, bookmarks: :user)
     else
       flash[:info] = ["退会済みのユーザーです"]
       redirect_to root_path
